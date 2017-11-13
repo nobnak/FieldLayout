b11232d5acf1ff99583539abf2a8647945350565
Nobuyuki Nakata <nobu.nakata@gmail.com>
2017-11-13 21:30:52 +0900

    backup

---
+++ Polygon2D.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Polyhedra2DZone {
+
+    [System.Serializable]
+    public class Polygon2D {
+
+        [SerializeField]
+        protected List<Vector2> vertices = new List<Vector2>();
+
+        #region List
+        public int Count { get { return vertices.Count; } }
+        public Vector2 this[int i] {
+            get { return vertices[i]; }
+            set { vertices[i] = value; }
+        }
+        public int Add(Vector2 v) {
+            var i = vertices.Count;
+            vertices.Add(v);
+            return i;
+        }
+        public Vector2 Remove(int i) {
+            var v = vertices[i];
+            vertices.RemoveAt(i);
+            return v;
+        }
+        #endregion
+
+        public IEnumerable<Vector2> IterateVertices() {
+            foreach (var v in vertices)
+                yield return v;
+        }
+        public IEnumerable<Vector2> IterateEdges() {
+            var count = vertices.Count;
+            for (var i = 0; i < count; i++) {
+                var j = (i + 1) % count;
+                yield return vertices[i];
+                yield return vertices[j];
+            }
+        }
+    }
+}
---
+++ PolygonMaker.cs
@@ -0,0 +1,60 @@
+﻿using Gist;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Polyhedra2DZone {
+    [ExecuteInEditMode]
+    public class PolygonMaker : MonoBehaviour {
+        public static readonly Vector2[] QUAD_VERTICES = new Vector2[] {
+            0.5f * new Vector2(-1,-1), 0.5f * new Vector2(-1,1),
+            0.5f * Vector2.one, 0.5f * new Vector2(1, -1)
+        };
+
+        [SerializeField] protected Polygon2D polygon;
+        [SerializeField] protected Color edgeColor = Color.green;
+        [SerializeField] protected Color quadColor = Color.grey;
+
+        protected GLMaterial glmat;
+
+        void OnEnable() {
+            glmat = new GLMaterial();
+        }
+        void OnDisable() {
+            glmat.Dispose();
+        }
+        void OnRenderObject() {
+            if (glmat == null)
+                return;
+
+            var view = Camera.current.worldToCameraMatrix;
+
+            GL.PushMatrix();
+            GL.LoadIdentity();
+            GL.MultMatrix(view * transform.localToWorldMatrix);
+
+            GL.Begin(GL.LINES);
+            glmat.Color(edgeColor);
+            foreach (var v in polygon.IterateEdges())
+                GL.Vertex(v);
+            GL.End();
+
+            GL.Begin(GL.LINES);
+            glmat.Color(quadColor);
+            foreach (var v in IterateQuadEdges())
+                GL.Vertex(v);
+            GL.End();
+
+            GL.PopMatrix();
+            
+        }
+
+
+        IEnumerable<Vector2> IterateQuadEdges() {
+            for (var i = 0; i < QUAD_VERTICES.Length; i++) {
+                var j = (i + 1) % QUAD_VERTICES.Length;
+                yield return QUAD_VERTICES[i];
+                yield return QUAD_VERTICES[j];
+            }
+        }
+    }
+}
