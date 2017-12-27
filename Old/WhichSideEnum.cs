f2bfa8e56d4089e0a9b2919ed7474cb78b5dc52f
Nakata Nobuyuki <nobu.nakata@gmail.com>
2017-12-28 06:07:53 +0900

    古いシステムを破棄して、名称も FieldLayout に変更

--- Old/WhichSideEnum.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-namespace Polyhedra2DZone {
-
-    public enum WhichSideEnum { Unknown = 0, Inside, Outside }
-
-}
