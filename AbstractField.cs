7be358f1dfd6d61961693305043e5553da76cfe4
Nakata Nobuyuki <nobu.nakata@gmail.com>
2017-12-27 20:42:46 +0900

    ベースクラスを設置

---
+++ AbstractField.cs
@@ -0,0 +1,72 @@
+﻿using Gist.Extensions.RectExt;
+using nobnak.Gist;
+using nobnak.Gist.Extensions.Behaviour;
+using nobnak.Gist.Extensions.ComponentExt;
+using nobnak.Gist.Layer2;
+using nobnak.Gist.Primitive;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace LevelDesign {
+
+    [ExecuteInEditMode]
+    public abstract class AbstractField : MonoBehaviour {
+
+        [SerializeField] protected Layer layer;
+        [Range(0f, 10f)]
+        [SerializeField] protected float borderThickness = 0.1f;
+
+        protected DefferedMatrix localToLayer = new DefferedMatrix();
+        protected Validator validator = new Validator();
+        protected GLFigure gl;
+
+        #region Unity
+        protected virtual void OnEnable() {
+            if (layer == null) {
+                enabled = false;
+                return;
+            }
+
+            gl = new GLFigure();
+
+            validator.Reset();
+            validator.Validation += () => {
+                layer.LayerValidator.CheckValidation();
+                Rebuild();
+                transform.hasChanged = false;
+            };
+            validator.SetCheckers(() => layer.LayerValidator.IsValid && !transform.hasChanged);
+            layer.LayerValidator.Invalidated += () => validator.Invalidate();
+        }
+        protected virtual void OnValidate() {
+            validator.Invalidate();
+        }
+        protected virtual void OnDisable() {
+            gl.Dispose();
+        }
+        #endregion
+
+        #region Message
+        protected virtual void CrownLayer(Layer layer) {
+            this.layer = layer;
+            if (layer != null)
+                enabled = true;
+        }
+        #endregion
+
+        public abstract SideEnum Side(Vector2 layerPoint);
+        public abstract Vector2 ClosestPoint(Vector2 layerPoint, SideEnum side = SideEnum.Inside);
+
+        protected abstract void Rebuild();
+
+        protected virtual bool CanRender {
+            get {
+                return layer != null 
+                    && this.IsActiveAndEnabledAlsoInEditMode()
+              && this.IsActiveLayer()
+              && validator.CheckValidation();
+            }
+        }
+    }
+}
--- Rectangle.cs
+++ Rectangle.cs
@@ -11,15 +11,9 @@ using UnityEngine;
 namespace LevelDesign {
 
     [ExecuteInEditMode]
-    public class Rectangle : MonoBehaviour {
+    public class Rectangle : AbstractField {
 
-        [SerializeField] protected Layer layer;
-        [Range(0f, 10f)]
-        [SerializeField] protected float borderThickness = 0.1f;
         [SerializeField] protected Rect localSize = new Rect(-0.5f, -0.5f, 1f, 1f);
-
-        protected DefferedMatrix localToLayer = new DefferedMatrix();
-        protected Validator validator = new Validator();
 
         protected Rect layerInside;
         protected Vector2 layerInsideMin;
@@ -28,30 +22,9 @@ namespace LevelDesign {
         protected Vector2 layerOutsideMin;
         protected Vector2 layerOutsideMax;
 
-        protected GLFigure gl;
-
         #region Unity
-        void OnEnable() {
-            if (layer == null) {
-                enabled = false;
-                return;
-            }
-
-            gl = new GLFigure();
-
-            validator.Reset();
-            validator.Validation += () => {
-                layer.LayerValidator.CheckValidation();
-                Rebuild();
-                transform.hasChanged = false;
-            };
-            validator.SetCheckers(() => layer.LayerValidator.IsValid && !transform.hasChanged);
-            layer.LayerValidator.Invalidated += () => validator.Invalidate();
-        }
-        void OnRenderObject() {
-            if (!this.IsActiveAndEnabledAlsoInEditMode()
-                || !this.IsActiveLayer()
-                || !validator.CheckValidation())
+        protected virtual void OnRenderObject() {
+            if (!CanRender)
                 return;
 
             var view = Camera.current.worldToCameraMatrix * layer.LayerToWorld.Matrix;
@@ -62,23 +35,9 @@ namespace LevelDesign {
             gl.DrawQuad(view * Matrix4x4.TRS(
                 layerOutside.center, Quaternion.identity, layerOutside.size));
         }
-        void OnValidate() {
-            validator.Invalidate();
-        }
-        void OnDisable() {
-            gl.Dispose();
-        }
         #endregion
-
-        #region Message
-        protected virtual void CrownLayer(Layer layer) {
-            this.layer = layer;
-            if (layer != null)
-                enabled = true;
-        }
-        #endregion
-
-        public SideEnum Side(Vector2 layerPoint) {
+        
+        public override SideEnum Side(Vector2 layerPoint) {
             var outsideContained = Contains(layerOutsideMin, layerOutsideMax, layerPoint);
             if (!outsideContained)
                 return SideEnum.Outside;
@@ -87,7 +46,7 @@ namespace LevelDesign {
             return insideContained ? SideEnum.Inside : SideEnum.Border;
 
         }
-        public Vector2 ClosestPoint(Vector2 layerPoint, SideEnum side = SideEnum.Inside) {
+        public override Vector2 ClosestPoint(Vector2 layerPoint, SideEnum side = SideEnum.Inside) {
             switch (side) {
                 case SideEnum.Outside:
                     return layerOutside.ClosestPoint(layerPoint);
@@ -96,7 +55,7 @@ namespace LevelDesign {
             }
         }
 
-        protected void Rebuild() {
+        protected override void Rebuild() {
             localToLayer.Reset(layer.LocalToLayer.Matrix,
                 Matrix4x4.TRS(transform.localPosition, Quaternion.identity, transform.localScale));
 
