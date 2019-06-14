ac68aabdfb7c93cf43b31159ecf4b1dacc053323
Nakata Nobuyuki <nobu.nakata@gmail.com>
2019-06-14 18:17:04 +0900

    Extension for uv to world pos

--- Extension/FieldExtension.cs
+++ Extension/FieldExtension.cs
@@ -10,5 +10,8 @@ namespace nobnak.FieldLayout.Extensions {
 			var layerPos = af.LocalToLayer.TransformPoint(localPos);
 			return af.Layer.LayerToWorld.TransformPoint(layerPos);
 		}
+        public static Vector3 UvToWorldPos(this Field f, Vector2 uv, float z = 0f) {
+            return f.LocalToWorldPos(new Vector3(uv.x - .5f, uv.y - .5f, z));
+        }
 	}
 }
