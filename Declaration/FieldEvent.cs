ff5e13a55e9319ca3bcdd3830cd4d4684909cd20
Nakata Nobuyuki <nobu.nakata@gmail.com>
2019-07-24 19:39:05 +0900

    インターフェイス系をフォルダに整理

---
+++ Declaration/FieldEvent.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace nobnak.FieldLayout {
+
+    [System.Serializable]
+    public class FieldEvent : UnityEngine.Events.UnityEvent<Field> { }
+}
