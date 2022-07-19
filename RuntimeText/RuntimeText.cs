4027ab5fd5122021137f04dcd9d163eb07091b37
Nakata Nobuyuki <nobu.nakata@gmail.com>
2022-07-19 19:50:50 +0900

    Text オブジェクトを一覧に追加

--- RuntimeText/RuntimeText.cs
+++ RuntimeText/RuntimeText.cs
@@ -1,28 +1,37 @@
 using nobnak.Gist.Extensions.CameraExt;
 using nobnak.Gist.ObjectExt;
+using System.Linq;
 using TMPro;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 namespace nobnak.FieldLayout.RuntimeTextSystem {
 	[ExecuteInEditMode]
     public class RuntimeText : MonoBehaviour {
 		public const float HANDLE_SIZE_BASE_SIZE = 0.01f;
+		public const string ROOT_OBJ_NAME = "Text Holder";
 
-        public string text = "Sample";
+		public string text = "Sample";
         public float size = 20;
 		public Color color = Color.white;
 		public bool autoScale = true;
 		public Vector2 anchor = new Vector2(0f, 1f);
 		public Vector3 anchorPosition;
 
+		static GameObject root;
+
         protected TextMeshPro pro;
 
         #region unity
         protected void OnEnable() {
-            pro = new GameObject("Text").AddComponent<TextMeshPro>();
+			if (root == null) root = GameObject.Find(ROOT_OBJ_NAME);
+			if (root == null) root = new GameObject(ROOT_OBJ_NAME);
+
+			pro = new GameObject("Text").AddComponent<TextMeshPro>();
 			pro.enableWordWrapping = false;
 			pro.gameObject.hideFlags = HideFlags.DontSave;
-			pro.gameObject.hideFlags |= HideFlags.HideInHierarchy;
+			//pro.gameObject.hideFlags |= HideFlags.HideInHierarchy;
+			pro.transform.SetParent(root.transform, true);
         }
 		protected void OnDisable() {
 			pro.DestroyGo();
